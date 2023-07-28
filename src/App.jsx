20496f5db468a9edde66a18ea043edf346ba88ec
Carlos David <carlos.monier87@gmail.com>
2023-07-27 19:58:00 -0600

    2D

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './components/login';
-import BuscarExpediente from './components/Dashboard';
-import InsertarExpediente from './components/ExpedienteForm';
+import Dashboard from './components/Dashboard';
+import ExpedienteForm from './components/expedienteForm'
+
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Login} />
-        <Route exact path="/PageContent" component={PageContent} />
-        <Route exact path="/menu" component={Menu} />
-        <Route exact path="/buscar" component={BuscarExpediente} />
-        <Route exact path="/insertar" component={InsertarExpediente} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/expedienteForm" component={ExpedienteForm} />
       </Switch>
     </BrowserRouter>
   );
