debad857f058cb32725d7db3365e7b10d6c26a49
Carlos David <carlos.monier87@gmail.com>
2023-07-27 23:31:53 -0600

    login Craado

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import DefaultLayout from "../layout/DefaultLayout";
 import { useAuth } from "../auth/AuthProvider";
-import {Navigate, useNavigate} from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { AuthResponse, AuthResponseError } from "../types/types";
+import "./login.css"; // Import the CSS file
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileAlt, faArchive, faFolder } from "@fortawesome/free-solid-svg-icons";
+import companyLogo from '../../public/LOGO.jpg'
 
 export default function Login() {
   const [username, setUsername] = useState("");
@@ -12,8 +16,8 @@ export default function Login() {
   const auth = useAuth();
   const goTo = useNavigate();
 
-  function handleChange(e: React.ChangeEvent) {
-    const { name, value } = e.target as HTMLInputElement;
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
     if (name === "username") {
       setUsername(value);
     }
@@ -24,13 +28,15 @@ export default function Login() {
 
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    // auth.setIsAuthenticated(true);
+
+    // Set the user as authenticated without checking the password
     try {
       const response = await fetch("http://localhost:3001/usuarios", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
       });
+
       if (response.ok) {
         const json = (await response.json()) as AuthResponse;
         console.log(json);
@@ -40,37 +46,45 @@ export default function Login() {
         }
       } else {
         const json = (await response.json()) as AuthResponseError;
-
         setErrorResponse(json.body.error);
       }
     } catch (error) {
       console.log(error);
     }
   }
+
   if (auth.isAuthenticated) {
     return <Navigate to="/dashboard" />;
   }
+
   return (
     <DefaultLayout>
       <form onSubmit={handleSubmit} className="form">
-        <h1>Login</h1>
+        <div className="company-logo">
+          <img src={companyLogo} alt="LOGO" />
+        </div>
+        <h1 className="titulo-verde">Sistema de Gestión Archivística</h1>
         {!!errorResponse && <div className="errorMessage">{errorResponse}</div>}
-        <label>Username</label>
+        <label>Usuario</label>
         <input
           name="username"
           type="text"
           onChange={handleChange}
           value={username}
         />
-        <label>Password</label>
+        <label>Contraseña</label>
         <input
           type="password"
           name="password"
           onChange={handleChange}
           value={password}
         />
-
-        <button>Login</button>
+        <button className="btn-entrar">Entrar</button>
+        <div className="document-management-icons">
+          <FontAwesomeIcon icon={faFileAlt} />
+          <FontAwesomeIcon icon={faArchive} />
+          <FontAwesomeIcon icon={faFolder} />
+        </div>
       </form>
     </DefaultLayout>
   );
